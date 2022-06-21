f4cd1bd5464d1cea1567715f8b96db7909ee0207
SRS <stian.schikora@nav.no>
2022-06-21 10:40:56 +0200

    Updated conv note display

--- force-app/main/default/lwc/diaConvNoteViewer/diaConvNoteViewer.js
+++ force-app/main/default/lwc/diaConvNoteViewer/diaConvNoteViewer.js
@@ -1,5 +1,23 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getconvnote from '@salesforce/apex/DIA_ThreadViewController.getConvNote';
 
 export default class DiaConvNoteViewer extends LightningElement {
     @api recordId;
+
+    conversationNote;
+    error = false;
+
+    @wire(getconvnote, { convNoteId: '$recordId' }) //Calls apex and gets conversation note record
+    wireNotes(result) {
+        if (result.error) {
+            this.error = true;
+            console.log('Error: ' + JSON.stringify(error, null, 2));
+        } else if (result.data) {
+            this.conversationNote = result.data[0];
+        }
+    }
+
+    get isLoading() {
+        return !this.conversationNote && this.error === false;
+    }
 }
