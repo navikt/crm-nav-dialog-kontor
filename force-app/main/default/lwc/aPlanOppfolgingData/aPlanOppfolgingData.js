da2ad34a62d0b03870f4e6072f7449d7d463ba2f
eirikfladby <eirik.fladby@gmail.com>
2022-09-05 15:58:31 +0200

    strip

--- force-app/main/default/lwc/aPlanOppfolgingData/aPlanOppfolgingData.js
+++ force-app/main/default/lwc/aPlanOppfolgingData/aPlanOppfolgingData.js
@@ -1,26 +1,20 @@
-import { LightningElement, wire, api } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
+import { wire, api } from 'lwc';
 import getOppfolgingsInfo from '@salesforce/apex/DIA_AktivitetsplanController.getOppfolgingsInfo';
 import CASE_ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
 import ACCOUNT_ID_FIELD from '@salesforce/schema/Account.Id';
 import ACCOUNT_PERSON_FIELD from '@salesforce/schema/Account.CRM_Person__c';
-import PERSON_IDENT_FIELD from '@salesforce/schema/Person__c.Name';
 import PERSON_ACTORID_FIELD from '@salesforce/schema/Person__c.INT_ActorId__c';
-import PERSON_FIRST_NAME from '@salesforce/schema/Person__c.INT_FirstName__c';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
-export default class APlanOppfolgingData extends NavigationMixin(LightningElement) {
-    @api actorId;
+export default class APlanOppfolgingData {
+    //@api actorId;
     @api recordId;
     @api objectApiName;
     accountField;
-    url;
     veileder;
     oppfolgingsenhet;
     underOppfolging = false;
-    firstName;
     actorId;
-    name;
     isLoading = false;
 
     personId;
@@ -40,40 +34,6 @@ export default class APlanOppfolgingData extends NavigationMixin(LightningElemen
         }
     }
 
-    viewOppfolging(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        this.aktivitetsPageRef = {
-            type: 'standard__navItemPage',
-            attributes: {
-                apiName: 'Aktivitetsplan'
-            },
-            state: {
-                c__ActorId: this.actorId,
-                c__firstName: this.firstName,
-                c__pName: this.name
-            }
-        };
-        this[NavigationMixin.GenerateUrl](this.aktivitetsPageRef).then((url) => (this.url = url));
-        this[NavigationMixin.Navigate](this.aktivitetsPageRef);
-    }
-
-    viewMeldekort(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        this.aktivitetsPageRef = {
-            type: 'standard__navItemPage',
-            attributes: {
-                apiName: 'NKS_Arenaytelser'
-            },
-            state: {
-                c__personId: this.personId
-            }
-        };
-        this[NavigationMixin.GenerateUrl](this.aktivitetsPageRef).then((url) => (this.url = url));
-        this[NavigationMixin.Navigate](this.aktivitetsPageRef);
-    }
-
     @wire(getOppfolgingsInfo, { actorId: '$actorId' })
     wireOppInfo({ data, error }) {
         if (data) {
@@ -91,27 +51,19 @@ export default class APlanOppfolgingData extends NavigationMixin(LightningElemen
         recordId: '$accountId',
         fields: [ACCOUNT_PERSON_FIELD]
     })
-    wiredAccountInfo({ error, data }) {
+    wiredAccountInfo({ data }) {
         if (data) {
             this.personId = getFieldValue(data, ACCOUNT_PERSON_FIELD);
-        }
-
-        if (error) {
         }
     }
 
     @wire(getRecord, {
         recordId: '$personId',
-        fields: [PERSON_ACTORID_FIELD, PERSON_IDENT_FIELD, PERSON_FIRST_NAME]
+        fields: [PERSON_ACTORID_FIELD]
     })
-    wiredPersonInfo({ error, data }) {
+    wiredPersonInfo({ data }) {
         if (data) {
             this.actorId = getFieldValue(data, PERSON_ACTORID_FIELD);
-            this.name = getFieldValue(data, PERSON_IDENT_FIELD);
-            this.firstName = getFieldValue(data, PERSON_FIRST_NAME);
-        }
-
-        if (error) {
         }
     }
 
@@ -119,13 +71,11 @@ export default class APlanOppfolgingData extends NavigationMixin(LightningElemen
         recordId: '$recordId',
         fields: '$accountField'
     })
-    wiredRecordInfo({ error, data }) {
+    wiredRecordInfo({ data }) {
         if (data) {
             if (this.accountField) {
                 this.accountId = getFieldValue(data, this.accountField[0]);
             }
-        }
-        if (error) {
         }
     }
 
@@ -141,9 +91,5 @@ export default class APlanOppfolgingData extends NavigationMixin(LightningElemen
 
     get ikkeUnderOppfolging() {
         return !this.underOppfolging;
-    }
-
-    get oppfolgingIconVariant() {
-        return this.underOppfolging === true ? '' : 'inverse';
     }
 }
